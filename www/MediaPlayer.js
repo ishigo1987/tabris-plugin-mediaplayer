880a25796594d0a4494058af1020d477da553f8a
Artyom Davydov <fax1ty.ru@gmail.com>
2020-06-01 15:40:06 +0400

    www part

--- www/MediaPlayer.js
+++ www/MediaPlayer.js
@@ -1,68 +1,66 @@
-var PLUGIN_ID = 'tabris-plugin-mediaplayer';
-
-var MediaPlayer = tabris.NativeObject.extend('ru.fax1ty.tabris.mediaplayer.MediaPlayer');
-
-MediaPlayer.prototype._listen = function (name, listening) {
-  this._nativeListen(name, listening);
-};
-
-MediaPlayer.prototype._trigger = function (name, event) {
-  tabris.Widget.prototype._trigger.call(this, name, event);
-};
-
-Object.assign(MediaPlayer.prototype, {
-  _nativeCreate: function () {
-    tabris.NativeObject.prototype._nativeCreate.apply(this, arguments);
-    return this;
-  },
-
-  play: function () {
-    this._nativeCall('play');
-  },
-
-  pause: function () {
-    this._nativeCall('pause');
-  },
-
-  stop: function () {
-    this._nativeCall('stop');
-  },
-
-  seekTo: function (position) {
-    if (typeof position != 'number') throw 'Argument "position" must be a number';
-    this._nativeCall('seekTo', { position: position });
-  },
-
-  getDuration: function () {
-    this._nativeCall('getDuration');
-  },
-
-  isPlaying: function () {
-    this._nativeCall('isPlaying');
-  },
-
-  isLooping: function () {
-    this._nativeCall('isLooping');
-  },
-
-  setIsLooping: function (loop) {
-    if (typeof loop != 'boolean') throw 'Argument "loop" must be a boolean';
-    else this._nativeCall('setIsLooping', { loop: loop });
-  },
-
-  getCurrentPosition: function () {
-    this._nativeCall('getCurrentPosition', { loop: loop });
-  },
-
-  dispose: function () {
-    this._dispose();
+class MediaPlayer extends tabris.NativeObject {
+  constructor(properties) {
+    super(properties);
   }
-});
 
-tabris.NativeObject.defineProperties(Map.prototype, {
-  url: { type: 'string', nocache: true },
-  loop: { type: 'boolean', nocache: true },
-  autoPlay: { type: 'boolean', default: false }
+  get _nativeType() {
+    return 'ru.fax1ty.tabris.mediaplayer.MediaPlayer';
+  }
+
+  play() {
+    this._nativeCall('play');
+  }
+
+  pause() {
+    this._nativeCall('pause');
+  }
+
+  stop() {
+    this._nativeCall('stop');
+  }
+
+  seekTo(position) {
+    if (typeof position != 'number') throw new Error('To call this method you must to pass number argument "position", but you passed ' + position);
+    else this._nativeCall('seekTo', { position: position });
+  }
+
+  getDuration() {
+    return this._nativeCall('getDuration');
+  }
+
+  isPlaying() {
+    return this._nativeCall('isPlaying');
+  }
+
+  isLooping() {
+    return this._nativeCall('isLooping');
+  }
+
+  setIsLooping(loop) {
+    if (typeof loop != 'boolean') throw new Error('To call this method you must to pass boolean argument "loop", but you passed ' + loop);
+    this._nativeCall('setIsLooping', { loop: loop });
+  }
+
+  getCurrentPosition() {
+    return this._nativeCall('getCurrentPosition');
+  }
+
+  _trigger(name, event) {
+    return super._trigger(name, event);
+  }
+
+  _listen(name, listening) {
+    if (name === 'error' || name === 'ready') {
+      this._nativeListen(name, listening);
+    } else {
+      super._listen(name, listening);
+    }
+  }
+}
+
+tabris.NativeObject.defineEvents(MediaPlayer.prototype, {
+  error: { native: true },
+  ready: { native: true },
 });
 
 module.exports = MediaPlayer;
