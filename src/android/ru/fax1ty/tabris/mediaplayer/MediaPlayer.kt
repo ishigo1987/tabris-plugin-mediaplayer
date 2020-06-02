1b69dff001d4b5317af542e24a2bccee836eefd2
Artyom Davydov <fax1ty.ru@gmail.com>
2020-06-02 14:54:45 +0400

    Nullability

--- src/android/ru/fax1ty/tabris/mediaplayer/MediaPlayer.kt
+++ src/android/ru/fax1ty/tabris/mediaplayer/MediaPlayer.kt
@@ -5,39 +5,39 @@ import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.net.Uri
 
-class MediaPlayer(activity: Activity, url: String, private val loop: Boolean, private val autoPlay: Boolean) {
+class MediaPlayer(activity: Activity, url: String, loop: Boolean, autoPlay: Boolean) {
 
-    var mediaPlayer = MediaPlayer.create(activity.applicationContext, Uri.parse(url)).apply {
+    var mediaPlayer: MediaPlayer? = MediaPlayer.create(activity.applicationContext, Uri.parse(url)).apply {
         setAudioAttributes(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build())
         isLooping = loop
         if (autoPlay) start()
     }
 
     fun play() {
-        if (!mediaPlayer.isPlaying) mediaPlayer.start()
+        if (!mediaPlayer!!.isPlaying) mediaPlayer?.start()
     }
 
     fun pause() {
-        if (mediaPlayer.isPlaying) mediaPlayer.pause()
+        if (mediaPlayer!!.isPlaying) mediaPlayer?.pause()
     }
 
     fun stop() {
-        if (mediaPlayer.isPlaying) mediaPlayer.stop()
+        if (mediaPlayer!!.isPlaying) mediaPlayer?.stop()
     }
 
     fun seekTo(position: Int) {
-        mediaPlayer.seekTo(position)
+        mediaPlayer?.seekTo(position)
     }
 
-    fun getDuration() = mediaPlayer.duration
+    fun getDuration() = mediaPlayer?.duration
 
-    fun isPlaying() = mediaPlayer.isPlaying
+    fun isPlaying() = mediaPlayer?.isPlaying
 
-    fun isLooping() = mediaPlayer.isLooping
+    fun isLooping() = mediaPlayer?.isLooping
 
     fun setIsLooping(loop: Boolean) {
-        mediaPlayer.isLooping = loop
+        mediaPlayer?.isLooping = loop
     }
 
-    fun getCurrentPosition() = mediaPlayer.currentPosition
+    fun getCurrentPosition() = mediaPlayer?.currentPosition
 }
--- src/android/ru/fax1ty/tabris/mediaplayer/MediaPlayerHandler.kt
+++ src/android/ru/fax1ty/tabris/mediaplayer/MediaPlayerHandler.kt
@@ -2,6 +2,8 @@ package ru.fax1ty.tabris.mediaplayer
 
 import com.eclipsesource.tabris.android.ActivityScope
 import com.eclipsesource.tabris.android.ObjectHandler
+import com.eclipsesource.tabris.android.internal.ktx.getBooleanOrNull
+import com.eclipsesource.tabris.android.internal.ktx.getStringOrNull
 import com.eclipsesource.v8.V8Object
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
@@ -9,7 +11,9 @@ class MediaPlayerHandler(private val scope: ActivityScope) : ObjectHandler<Media
 
     override val type = "ru.fax1ty.tabris.mediaplayer.MediaPlayer"
 
-    override fun create(id: String, properties: V8Object) = MediaPlayer(scope.activity, properties.getString("url"), properties.getBoolean("loop"), properties.getBoolean("autoPlay"))
+    override fun create(id: String, properties: V8Object) = MediaPlayer(scope.activity, properties.getStringOrNull("url")
+            ?: "", properties.getBooleanOrNull("loop")
+            ?: false, properties.getBooleanOrNull("autoPlay") ?: false)
 
     override fun call(mediaPlayer: MediaPlayer, method: String, properties: V8Object): Any? = when (method) {
         "play" -> mediaPlayer.play()
@@ -30,26 +34,26 @@ class MediaPlayerHandler(private val scope: ActivityScope) : ObjectHandler<Media
         super.listen(id, mediaPlayer, event, listen)
         when (event) {
             "error" -> if (listen) {
-                mediaPlayer.mediaPlayer.setOnErrorListener(MediaPlayerErrorListener(mediaPlayer, scope))
+                mediaPlayer.mediaPlayer?.setOnErrorListener(MediaPlayerErrorListener(mediaPlayer, scope))
             } else {
-                mediaPlayer.mediaPlayer.setOnErrorListener(null)
+                mediaPlayer.mediaPlayer?.setOnErrorListener(null)
             }
             "ready" -> if (listen) {
-                mediaPlayer.mediaPlayer.setOnPreparedListener(MediaPlayerReadyListener(mediaPlayer, scope))
+                mediaPlayer.mediaPlayer?.setOnPreparedListener(MediaPlayerReadyListener(mediaPlayer, scope))
             } else {
-                mediaPlayer.mediaPlayer.setOnPreparedListener(null)
+                mediaPlayer.mediaPlayer?.setOnPreparedListener(null)
             }
             "complete" -> if (listen) {
-                mediaPlayer.mediaPlayer.setOnCompletionListener(MediaPlayerCompleteListener(mediaPlayer, scope))
+                mediaPlayer.mediaPlayer?.setOnCompletionListener(MediaPlayerCompleteListener(mediaPlayer, scope))
             } else {
-                mediaPlayer.mediaPlayer.setOnCompletionListener(null)
+                mediaPlayer.mediaPlayer?.setOnCompletionListener(null)
             }
         }
     }
 
     override fun destroy(mediaPlayer: MediaPlayer) {
         super.destroy(mediaPlayer)
-        mediaPlayer.mediaPlayer.release()
+        mediaPlayer.mediaPlayer?.release()
         mediaPlayer.mediaPlayer = null
     }
 
