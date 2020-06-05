80774ede6a9a2b5d00439cdf17edea004cc60d03
Artyom Davydov <fax1ty.ru@gmail.com>
2020-06-05 13:32:20 +0400

    local files

--- src/android/ru/fax1ty/tabris/mediaplayer/MediaPlayer.kt
+++ src/android/ru/fax1ty/tabris/mediaplayer/MediaPlayer.kt
@@ -4,10 +4,11 @@ import android.app.Activity
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.net.Uri
+import java.io.File
 
 class MediaPlayer(activity: Activity, url: String, loop: Boolean, autoPlay: Boolean) {
 
-    var mediaPlayer: MediaPlayer? = MediaPlayer.create(activity.applicationContext, Uri.parse(url)).apply {
+    var mediaPlayer: MediaPlayer? = MediaPlayer.create(activity.applicationContext, if (url.contains("http", true)) Uri.parse(url) else Uri.fromFile(File(url))).apply {
         setAudioAttributes(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build())
         isLooping = loop
         if (autoPlay) start()
